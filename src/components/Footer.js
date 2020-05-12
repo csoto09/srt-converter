6959be857e10808ce12f41008587935fb57d6db5
Carlos J. Soto <csoto09@gmail.com>
2020-05-12 11:22:28 -0400

    Added Bulma Attribution, also changed title & logo

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import bulmaLogo from "../made-with-bulma.png"
+
+const Footer = (props) => (
+  <footer className='footer'>
+    <div className='content has-text-centered'>
+      <a href='https://bulma.io'>
+        <img src={bulmaLogo} alt='Made with Bulma' width='128' height='24' />
+      </a>
+    </div>
+  </footer>
+)
+
+export default Footer
